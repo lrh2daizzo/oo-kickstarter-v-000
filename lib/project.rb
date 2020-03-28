2d67282f568490d47b497561b368f763c7f1cb70
Luis Herrera <herrera.lrh@gmail.com>
2020-03-28 19:05:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,15 @@
 class Project
   attr_accessor :title
+  attr_reader :backers
 
   def initialize(title)
     @title = title
+    @backers = []
   end
 
   def add_backer(backer)
     backer.back_project(self)
+    self.backers << backer
   end
 
 end
